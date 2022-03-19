4043a80c854ecdc21223353f316cbc542598fda3
Teo <aktr9886@protonmail.com>
2022-03-19 11:23:19 +0200

    updated connect

--- connect.php
+++ connect.php
@@ -5,7 +5,7 @@ function getConnection()
 {
     try {
         $dsn = "mysql:host=localhost;dbname=fiipractic";
-        $connection = new PDO($dsn, "root", "root");
+        $connection = new PDO($dsn, "root", "Te0d0r4!");
     }catch (PDOException $exception) {
         echo "CANNOT_CONNECT_TO_DB";
         exit;
